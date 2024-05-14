a0481a5dd6e9f7c801a639382df84cbd5ebd9737
kroekkarawit <kroekarawit@gmail.com>
2024-05-14 15:20:22 +0700

    fix: port

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use('/users', usersRouter);
 app.use('/admins', adminsRouter);
 
 // Start the server
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
