84d6493dba05f64626dd9ecc0d49e7d1a0672d09
kroekkarawit <kroekkarawit@gmail.com>
2024-05-31 22:29:26 +0700

    fix: date bet

--- routes/bet.js
+++ routes/bet.js
@@ -1,141 +1,164 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
-const verifyToken = require('../utils/jwt');
-const convertBets = require('../utils/tools');
-const convertToSlipFormat = require('../utils/slip-gen');
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const verifyToken = require("../utils/jwt");
+const convertBets = require("../utils/tools");
+const convertToSlipFormat = require("../utils/slip-gen");
 
-router.post('/', async (req, res, next) => {
-    const { bet, currency } = req.body;
-    if (!bet) {
-        return res.status(400).json({ message: 'bet detail are required' });
-    }
+router.post("/", async (req, res, next) => {
+  const { bet, currency } = req.body;
+  if (!bet) {
+    return res.status(400).json({ message: "bet detail are required" });
+  }
 
-    const decodedToken = verifyToken(req);
-    const userId = decodedToken.id.toString();
+  const decodedToken = verifyToken(req);
+  const userId = decodedToken.id.toString();
 
-    const user = await prisma.user.findUnique({
-        where: {
-            id: parseInt(userId),
-        }
+  const user = await prisma.user.findUnique({
+    where: {
+      id: parseInt(userId),
+    },
+  });
+
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
+  }
+
+  console.log(userId, user);
+
+  try {
+    const newReceipt = await prisma.receipt.create({
+      data: {
+        user_id: parseInt(userId),
+        remark: "",
+        currency: currency,
+        bet_method: "MULTIPLY",
+        total_amount: 0,
+        ip_address: user?.ip_address || null,
+        status: "PENDING",
+      },
     });
 
-    if (!user) {
-        return res.status(404).json({ message: 'User not found' });
-    }
+    let totalAmount = 0;
+    const betData = bet;
+    const convertedBets = convertBets(betData);
+    let betPrepared = [];
 
-    console.log(userId, user);
+    convertedBets.forEach((bet) => {
+      if (!bet.number) return;
 
-    try {
-        const newReceipt = await prisma.receipt.create({
-            data: {
-                user_id: parseInt(userId),
-                remark: "",
-                currency: currency,
-                bet_method: "MULTIPLY",
-                total_amount: 0,
-                ip_address: user?.ip_address || null,
-                status: "PENDING"
-            }
-        });
+      Object.entries(bet.bet_type).forEach(([bet_type, bet_amount]) => {
+        if (!bet_amount) return;
 
-        let totalAmount = 0;
-        const betData = bet
-        const convertedBets = convertBets(betData);
-        let betPrepared = [];
+        Object.entries(bet.lotto_type).forEach(
+          ([lottery_type, lottery_value]) => {
+            if (!lottery_value) return;
 
-        convertedBets.forEach(bet => {
-            if (!bet.number) return;
-
-            Object.entries(bet.bet_type).forEach(([bet_type, bet_amount]) => {
-                if (!bet_amount) return;
-
-                Object.entries(bet.lotto_type).forEach(([lottery_type, lottery_value]) => {
-                    if (!lottery_value) return;
-
-                    Object.entries(bet.date).forEach(([date, date_value]) => {
-                        if (date_value) {
-                            console.log(`Number: ${bet.number}, Bet Type: ${bet_type}, Bet Amount: ${bet_amount}, Lottery Type: ${lottery_type}, Date: ${date} \n`);
-                            totalAmount += parseFloat(bet_amount);
-                            betPrepared.push({
-                                user_id: parseInt(userId),
-                                receipt_id: newReceipt.id.toString(),
-                                number: bet.number,
-                                amount: parseFloat(bet_amount).toFixed(2),
-                                bet_type: bet_type,
-                                lottery_type: lottery_type,
-                                status: "PENDING",
-                                result_date: new Date(date)
-                            })
-                        }
-                    });
+            Object.entries(bet.date).forEach(([date, date_value]) => {
+              if (date_value) {
+                console.log(
+                  `Number: ${bet.number}, Bet Type: ${bet_type}, Bet Amount: ${bet_amount}, Lottery Type: ${lottery_type}, Date: ${date} \n`
+                );
+                totalAmount += parseFloat(bet_amount);
+                betPrepared.push({
+                  user_id: parseInt(userId),
+                  receipt_id: newReceipt.id.toString(),
+                  number: bet.number,
+                  amount: parseFloat(bet_amount).toFixed(2),
+                  bet_type: bet_type,
+                  lottery_type: lottery_type,
+                  status: "PENDING",
+                  result_date: new Date(date),
                 });
+              }
             });
-        });
-        if (betPrepared.length <= 0) {
-            return res.status(400).json({ message: 'bet is empty' });
-        }
-
-        const createBets = await prisma.bet.createMany({
-            data: betPrepared,
-            skipDuplicates: false
-        });
-
-        const updateReceipt = await prisma.receipt.update({
-            where: {
-                id: newReceipt.id,
-            },
-            data: {
-                total_amount: totalAmount,
-            },
-        });
-
-        const updateUser = await prisma.user.update({
-            where: {
-                id: parseInt(userId),
-            },
-            data: {
-                credit: user.credit - totalAmount,
-            },
-        });
-
-        res.json({
-            detail: convertToSlipFormat(req.body, new Date(), user.username)
-        })
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Internal server error', details: error.message });
+          }
+        );
+      });
+    });
+    if (betPrepared.length <= 0) {
+      return res.status(400).json({ message: "bet is empty" });
     }
+
+    const createBets = await prisma.bet.createMany({
+      data: betPrepared,
+      skipDuplicates: false,
+    });
+
+    const updateReceipt = await prisma.receipt.update({
+      where: {
+        id: newReceipt.id,
+      },
+      data: {
+        total_amount: totalAmount,
+      },
+    });
+
+    const updateUser = await prisma.user.update({
+      where: {
+        id: parseInt(userId),
+      },
+      data: {
+        credit: user.credit - totalAmount,
+      },
+    });
+    function formatDate(date) {
+      const year = date.getFullYear().toString().slice(-2); // Last two digits of the year
+      const month = (date.getMonth() + 1).toString().padStart(2, "0"); // Month (1-based and padded with 0)
+      const day = date.getDate().toString().padStart(2, "0"); // Day of the month padded with 0
+      const hours = date.getHours().toString().padStart(2, "0"); // Hours padded with 0
+      const minutes = date.getMinutes().toString().padStart(2, "0"); // Minutes padded with 0
+
+      return `${year}${month}${day} ${hours}${minutes}`;
+    }
+    res.json({
+      detail: convertToSlipFormat(
+        req.body,
+        formatDate(new Date()),
+        user.username
+      ),
+    });
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ error: "Internal server error", details: error.message });
+  }
 });
 
+router.post("/receipt", async (req, res, next) => {
+  const {
+    user_id,
+    currency,
+    draw_date: result_date,
+    bet_date: created_at,
+    page_number,
+  } = req.body;
 
-router.post('/receipt', async (req, res, next) => {
-    const { user_id, currency, draw_date: result_date, bet_date: created_at, page_number } = req.body;
+  const decodedToken = verifyToken(req);
+  const userId = decodedToken.id.toString();
 
-    const decodedToken = verifyToken(req);
-    const userId = decodedToken.id.toString();
+  const user = await prisma.user.findFirst({
+    where: {
+      id: parseInt(userId),
+    },
+  });
+  console.log("user", user);
+  try {
+    let where = {};
 
-    const user = await prisma.user.findFirst({
-        where: {
-            id: parseInt(userId),
-        }
-    });
-    console.log("user", user);
-    try {
-        let where = {};
+    if (user_id) {
+      where.user_id = parseInt(user_id);
+    }
 
-        if (user_id) {
-            where.user_id = parseInt(user_id);
-        }
-
-        if (currency) {
-            where.currency = currency;
-        }
-        //TODO: fixing this query cause result time is different
-        /*
+    if (currency) {
+      where.currency = currency;
+    }
+    //TODO: fixing this query cause result time is different
+    /*
                 if (result_date && result_date.start && result_date.end) {
                     where.result_date = {
                         gte: new Date(result_date.start),
@@ -143,22 +166,22 @@ router.post('/receipt', async (req, res, next) => {
                     };
                 }
         */
-        if (created_at && created_at.start && created_at.end) {
-            where.created_at = {
-                gte: new Date(created_at.start),
-                lte: new Date(created_at.end),
-            };
-        }
-        console.log(where)
-        const receipts = await prisma.receipt.findMany({
-            where,
-        });
+    if (created_at && created_at.start && created_at.end) {
+      where.created_at = {
+        gte: new Date(created_at.start),
+        lte: new Date(created_at.end),
+      };
+    }
+    console.log(where);
+    const receipts = await prisma.receipt.findMany({
+      where,
+    });
 
-        const preReceipts = receipts.map((i) => {
-            return {
-                ...i,
-                accounts: { username: "kp3773", name: "xiaopang" },
-                bet_info: `Page: ${page_number}
+    const preReceipts = receipts.map((i) => {
+      return {
+        ...i,
+        accounts: { username: "kp3773", name: "xiaopang" },
+        bet_info: `Page: ${page_number}
         Currency: ${i.currency}
         Date/Time: ${new Date(i.created_at).toLocaleDateString()}
         Bet By: ${user.username} (${user.name})
@@ -170,7 +193,7 @@ router.post('/receipt', async (req, res, next) => {
         Box / IBox: * / **
         Draw Date / Day: # / ##`,
 
-                slip: `240515 1251 (4)
+        slip: `240515 1251 (4)
         kp3773
         15/05-18/05
         *MSBH
@@ -178,24 +201,21 @@ router.post('/receipt', async (req, res, next) => {
         GT=11.20 (SGD)
         GT=39.20 (MYR)
         
-        Points:0013 3601 3830 1733`
-
-            }
-        })
-        res.json(preReceipts);
-
-
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Internal server error', details: error.message });
-    }
+        Points:0013 3601 3830 1733`,
+      };
+    });
+    res.json(preReceipts);
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ error: "Internal server error", details: error.message });
+  }
 });
 
-
-
-process.on('SIGINT', async () => {
-    await prisma.$disconnect();
-    process.exit();
+process.on("SIGINT", async () => {
+  await prisma.$disconnect();
+  process.exit();
 });
 
 module.exports = router;
